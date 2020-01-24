445efec359622e4273c56883ad14d6415ed0022d
Ugonz86 <ugonzalez86@gmail.com>
2020-01-24 12:33:54 -0800

    Add essential content in Controllers

--- Market/Controllers/FlavorsController.cs
+++ Market/Controllers/FlavorsController.cs
@@ -15,23 +15,44 @@ namespace Market.Controllers
   public class FlavorsController : Controller
   {
     
+private readonly MarketContext _db;
+
+    public FlavorsController(MarketContext db)
+    {
+      _db = db;
+    }
+
     public ActionResult Index()
     {
-      return View();
+      return View(_db.Flavors.ToList());
     }
 
     public ActionResult Create()
     {
-      
+    
+      ViewBag.FlavorList = _db.Flavors.ToList();
       return View();
     }
 
     [HttpPost]
     public ActionResult Create(Flavor flavor, int TreatId)
     {
-      
-    
+      _db.Flavors.Add(flavor);
+      if (TreatId != 0)
+      {
+        _db.TreatFlavor.Add(new TreatFlavor() { TreatId = TreatId, FlavorId = flavor.FlavorId });
+      }
+      _db.SaveChanges();
       return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisFlavor = _db.Flavors
+          .Include(flavor => flavor.Treats)
+          .ThenInclude(join => join.Treat)
+          .FirstOrDefault(flavor => flavor.FlavorId == id);
+      return View(thisFlavor);
     }
 
   }
--- Market/Controllers/TreatsController.cs
+++ Market/Controllers/TreatsController.cs
@@ -1,37 +1,48 @@
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authorization;
-using System;
-using System.Linq;
-using System.Collections.Generic;
 using Market.Models;
-using System.Security.Claims;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace Market.Controllers
 {
   public class TreatsController : Controller
   {
 
+      private readonly MarketContext _db;
+
+    public TreatsController(MarketContext db)
+    {
+      _db = db;
+    }
+
     public ActionResult Index()
     {
-      
-      return View();
+      List<Treat> model = _db.Treats.ToList();
+      return View(model);
     }
 
     public ActionResult Create()
     {
-     
+      ViewBag.TreatList = _db.Treats.ToList();
       return View();
     }
 
     [HttpPost]
     public ActionResult Create(Treat treat)
     {
-    
-      
+      _db.Treats.Add(treat);
+      _db.SaveChanges();
       return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisTreat = _db.Treats
+        .Include(treat => treat.Flavors)
+        .ThenInclude(join => join.Flavor)
+        .FirstOrDefault(treat => treat.TreatId == id);
+        return View(thisTreat);
     }
 
   }
